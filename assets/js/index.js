7e091121ede9406b2fbf35e8fc6fe969d2eff9a6
William Dias <smdwilliam89@gmail.com>
2024-03-25 12:02:24 -0300

    feat(all the files): Add the click function to change between dark-mode and light-mode.

---
+++ assets/js/index.js
@@ -0,0 +1,38 @@
+let darkModeSetting = document.getElementById('darkModeSetting');
+let changeBackground = document.querySelector('.breakfast-div');
+let changeTitle = document.querySelector('.title');
+let changeParagraph = document.querySelector('#breakfast p');
+let changeIcon = document.querySelector('.change-icon');
+let changeClass = document.querySelector('.fa-sun');
+
+let isDarkMode = true;
+
+darkModeSetting.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    if (isDarkMode) {
+        changeBackground.style.backgroundColor = 'white';
+        changeParagraph.style.color = 'black';
+        changeTitle.style.color = 'black';
+
+        // Change Icon style
+        changeIcon.style.borderColor = 'black';
+        changeIcon.style.color = 'black';
+
+        // Change Icon URL
+        changeClass.classList.replace('fa-sun', 'fa-moon');
+    } else {
+        changeBackground.style.backgroundColor = 'black';
+        changeParagraph.style.color = 'white';
+        changeTitle.style.color = 'white';
+
+        // Change Icon style
+        changeIcon.style.borderColor = 'white';
+        changeIcon.style.color = 'white';
+
+        // Change Icon URL
+        changeClass.classList.replace('fa-moon', 'fa-sun');
+    }
+
+    isDarkMode = !isDarkMode;
+});
