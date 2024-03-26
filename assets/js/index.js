f6429a705d258449a7903dc1eeaba6239a3eeac7
William Dias <smdwilliam89@gmail.com>
2024-03-25 21:24:56 -0300

    feat(style.css): Add new style to breafast section.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,38 +1,38 @@
-let darkModeSetting = document.getElementById('darkModeSetting');
-let changeBackground = document.querySelector('.breakfast-div');
-let changeTitle = document.querySelector('.title');
-let changeParagraph = document.querySelector('#breakfast p');
-let changeIcon = document.querySelector('.change-icon');
-let changeClass = document.querySelector('.fa-sun');
+// let darkModeSetting = document.getElementById('darkModeSetting');
+// let changeBackground = document.querySelector('.breakfast-div');
+// let changeTitle = document.querySelector('.title');
+// let changeParagraph = document.querySelector('#breakfast p');
+// let changeIcon = document.querySelector('.change-icon');
+// let changeClass = document.querySelector('.fa-moon');
 
-let isDarkMode = true;
+// let isDarkMode = true;
 
-darkModeSetting.addEventListener('click', function (e) {
-    e.preventDefault();
+// darkModeSetting.addEventListener('click', function (e) {
+//     e.preventDefault();
 
-    if (isDarkMode) {
-        changeBackground.style.backgroundColor = 'white';
-        changeParagraph.style.color = 'black';
-        changeTitle.style.color = 'black';
+//     if (isDarkMode) {
+//         changeBackground.style.backgroundColor = 'white';
+//         changeParagraph.style.color = 'black';
+//         changeTitle.style.color = 'black';
 
-        // Change Icon style
-        changeIcon.style.border = '1px solid black';
-        changeIcon.style.color = 'black';
+//         // Change Icon style
+//         changeIcon.style.border = '1px solid black';
+//         changeIcon.style.color = 'black';
 
-        // Change Icon URL
-        changeClass.classList.replace('fa-sun', 'fa-moon');
-    } else {
-        changeBackground.style.backgroundColor = 'black';
-        changeParagraph.style.color = 'white';
-        changeTitle.style.color = 'white';
+//         // Change Icon URL
+//         changeClass.classList.replace('fa-moon', 'fa-sun');
+//     } else {
+//         changeBackground.style.backgroundColor = 'black';
+//         changeParagraph.style.color = 'white';
+//         changeTitle.style.color = 'white';
 
-        // Change Icon style
-        changeIcon.style.border = '1px solid white';
-        changeIcon.style.color = 'white';
+//         // Change Icon style
+//         changeIcon.style.border = '1px solid white';
+//         changeIcon.style.color = 'white';
 
-        // Change Icon URL
-        changeClass.classList.replace('fa-moon', 'fa-sun');
-    }
+//         // Change Icon URL
+//         changeClass.classList.replace('fa-sun', 'fa-moon');
+//     }
 
-    isDarkMode = !isDarkMode;
-});
+//     isDarkMode = !isDarkMode;
+// });
